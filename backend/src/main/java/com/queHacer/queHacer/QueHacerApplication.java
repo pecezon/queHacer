a28a04a192eb0878336250c032373c3d413b563e
diegopez <lopez.diego@cetys.edu.mx>
2025-02-27 20:00:29 -0800

    Creando proyecto insano

---
+++ backend/src/main/java/com/queHacer/queHacer/QueHacerApplication.java
@@ -0,0 +1,13 @@
+package com.queHacer.queHacer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueHacerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueHacerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/queHacer/queHacer/QueHacerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queHacer.queHacer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueHacerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
