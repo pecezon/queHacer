f84d304fb11854ec86742d0eb3279f6fb6c85e0c
diegopez <lopez.diego@cetys.edu.mx>
2025-02-27 20:30:51 -0800

    env actualizado

--- backend/src/main/java/com/queHacer/queHacer/QueHacerApplication.java
+++ backend/src/main/java/com/queHacer/queHacer/QueHacerApplication.java
@@ -1,5 +1,6 @@
 package com.queHacer.queHacer;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QueHacerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USER", dotenv.get("DATABASE_USER"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+
 		SpringApplication.run(QueHacerApplication.class, args);
 	}
 
