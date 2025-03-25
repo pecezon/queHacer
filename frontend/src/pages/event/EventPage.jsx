b2e611738c1acb91a41286b8bc30c4d33cc8ee0f
jcpd2806 <crazyjsky2954@gmail.com>
2025-03-25 09:35:45 -0700

    gei

--- frontend/src/pages/event/EventPage.jsx
+++ frontend/src/pages/event/EventPage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { Nav } from "../../components/all/Nav";
 
 const EventPage = () => {
   const { id } = useParams();
   return (
     <div>
-      <h1>Event Page number {id}</h1>
-      <p>Welcome to the event page Ramiro gei!</p>
+      <div></div>
+      <div>
+        <h1>Event Page number {id}</h1>
+        <p>Welcome to the event page Ramiro gei!</p>
+      </div>
     </div>
   );
 };
