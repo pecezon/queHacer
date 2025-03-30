3a568ff56bb82dd5b908b77ffdfeb9a26819bd1e
diegopez <lopez.diego@cetys.edu.mx>
2025-03-29 17:58:56 -0700

    updating user + exception handling

--- backend/src/test/java/com/queHacer/queHacer/QueHacerApplicationTests.java
+++ backend/src/test/java/com/queHacer/queHacer/QueHacerApplicationTests.java
@@ -1,9 +1,17 @@
 package com.queHacer.queHacer;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"DATABASE_URL=${DATABASE_URL}",
+		"DATABASE_USER=${DATABASE_USER}",
+		"DATABASE_PASSWORD=${DATABASE_PASSWORD}"
+})
 class QueHacerApplicationTests {
 
 	@Test
