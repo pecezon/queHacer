55e6950b08e2c2a4dd91e6a1cb9cb83d393e002d
Diego López <lopez.diego@cetys.edu.mx>
2025-03-30 23:32:20 -0700

    signup and login pages modified

--- frontend/src/pages/auth/LogIn.jsx
+++ frontend/src/pages/auth/LogIn.jsx
@@ -1,17 +1,25 @@
 import React from "react";
-import { Input, Button } from "@heroui/react";
+import { Input, Button, Form } from "@heroui/react";
+import { Link } from "react-router-dom";
 
 const LogIn = () => {
   return (
     <div className="flex justify-center items-center h-screen w-screen bg-gradient-to-r from-indigo-200 to-blue-900">
-      <div className="flex flex-col justify-center items-center bg-white p-8 rounded-lg shadow-2xl w-3/4 gap-8 max-w-lg">
-        <h1 className="text-4xl font-bold text-center text-black">LogIn</h1>
+      <Form className="flex flex-col justify-center items-center bg-white p-8 rounded-3xl shadow-2xl w-3/4 gap-8 max-w-lg ">
+        <h1 className="text-3xl font-bold text-center text-black md:text-4xl">
+          LogIn
+        </h1>
         <Input label="Email" type="email"></Input>
         <Input label="Password" type="password"></Input>
-        <Button color="primary" variant="shadow">
+        <Button color="primary" variant="shadow" type="submit">
           LogIn
         </Button>
-      </div>
+        <div className="text-xs text-left w-full underline">
+          <Link to="/signup">
+            <p>You don't have an account yet? Sign Up</p>
+          </Link>
+        </div>
+      </Form>
     </div>
   );
 };
--- frontend/src/pages/auth/SignUp.jsx
+++ frontend/src/pages/auth/SignUp.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Input, Button, Form } from "@heroui/react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  return <div>Sign Up</div>;
+  return (
+    <div className="flex justify-center items-center h-screen w-screen bg-gradient-to-r from-indigo-200 to-blue-900">
+      <Form className="flex flex-col justify-center items-center bg-white p-8 rounded-3xl shadow-2xl w-3/4 gap-8 max-w-lg ">
+        <h1 className="text-3xl font-bold text-center text-black md:text-4xl">
+          SignUp
+        </h1>
+        <Input label="Email" type="email"></Input>
+        <Input label="Name" type="text"></Input>
+        <Input label="Lastname" type="text"></Input>
+        <Input
+          label="Phone Number"
+          type="tel"
+          pattern="/^\+?\d{1,3}[-.\s]?\(?\d{1,4}\)?[-.\s]?\d{3,4}[-.\s]?\d{3,4}$/"
+          errorMessage="Please enter a valid phone number"
+        ></Input>
+        <Input label="Password" type="password"></Input>
+        <Input label="Confirm your password" type="password"></Input>
+        <Button color="primary" variant="shadow" type="submit">
+          SignUp
+        </Button>
+        <div className="text-xs text-left w-full underline">
+          <Link to="/login">
+            <p>You already have an account? Log In</p>
+          </Link>
+        </div>
+      </Form>
+    </div>
+  );
 };
 
 export default SignUp;
