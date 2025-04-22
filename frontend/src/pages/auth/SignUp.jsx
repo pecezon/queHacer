3bec6e233856a3f423bb2df5e41704a38baa0fd1
JUAN PABLO CARDENAS <167574727+jcpd2806@users.noreply.github.com>
2025-04-21 20:34:57 -0700

    cambios login y sign up, quedando mas gua pito

--- frontend/src/pages/auth/SignUp.jsx
+++ frontend/src/pages/auth/SignUp.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Button, Input, Link } from '@heroui/react'
+import React from 'react'
 
-const SignUp = () => {
-  return <div>Sign Up</div>;
-};
+function SignUp() {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen bg-gradient-to-r from-indigo-200 to-blue-900">
+    <div className="flex flex-col justify-center items-center bg-white p-8 rounded-lg shadow-2xl w-3/4 gap-8 max-w-lg">
+      <h1 className="text-4xl font-bold text-center text-black">Sign Up</h1>
+      <Input label="Name" placeholder="Enter your name" type='name'></Input>
+      <Input label="Last Name" placeholder='Enter your last name' type='lastname'></Input>
+      <Input label="Email" placeholder="Enter your email" type="email"></Input>
+      <Input label="Password" placeholder="Enter your password" type="password"></Input>
+      <Input label="Confirm Password" placeholder='Confirm Password' type='password'></Input>
+      <Link href="/login" className="text-xs text-right self-end">Ya tienes cuenta?</Link>
+      <Button color="primary" variant="flat">
+        Sign Up
+      </Button>
+    </div>
+  </div>
+  )
+}
 
-export default SignUp;
+export default SignUp
--- frontend/src/pages/event/CardTickets.jsx
+++ frontend/src/pages/event/CardTickets.jsx
@@ -4,14 +4,19 @@ import React from 'react'
 function CardTIckets() {
   return (
     <div>
-        <Card>
-            <CardBody>
-                <h3>22 de marzo de 2025  Ensenada, Baja California</h3>
-                <Button>
-                    TicketMaster
-                </Button>
-            </CardBody>
-        </Card>
+      <Card>
+        <CardBody>
+          <h3>22 de marzo de 2025 · Ensenada, Baja California</h3>
+          <Button className="flex items-center gap-2 p-2 bg-white rounded-md shadow-md hover:bg-gray-100 transition">
+            <img
+              src="https://cdn.brandfetch.io/idVogTb1a1/w/800/h/800/theme/dark/icon.png?c=1dxbfHSJFAPEGdCLU4o5B"
+              alt="Ticketmaster Logo"
+              className="w-6 h-6 rounded-full"
+            />
+            <span className="text-black font-medium">Comprar boletos</span>
+          </Button>
+        </CardBody>
+      </Card>
     </div>
   )
 }
