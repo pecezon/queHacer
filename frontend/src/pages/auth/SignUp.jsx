9ae1f891f26464868fe9257be491eaff9224967c
Ramiro <rxlabrada@gmail.com>
2025-04-28 08:19:54 -0700

    Cambios de ramirousando git

--- frontend/src/pages/auth/SignUp.jsx
+++ frontend/src/pages/auth/SignUp.jsx
@@ -5,7 +5,8 @@ function SignUp() {
   return (
     <div className="flex justify-center items-center h-screen w-screen bg-gradient-to-r from-indigo-200 to-blue-900">
     <div className="flex flex-col justify-center items-center bg-white p-8 rounded-lg shadow-2xl w-3/4 gap-8 max-w-lg">
-      <h1 className="text-4xl font-bold text-center text-black">Sign Up</h1>
+      {/*<h1 className="text-4xl font-bold text-center text-black">Sign Up</h1>*/}
+      <img src="../images/logoQueHacerEn.svg" alt="Logo" className="w-24 h-auto"/>
       <Input label="Nombre" placeholder="Ingresa tu nombre" type='name'></Input>
       <Input label="Apellidos" placeholder='Ingresa tus apellidos' type='lastname'></Input>
       <Input label="Email" placeholder="Ingresa tu email" type="email"></Input>
--- frontend/src/pages/event/CardTickets.jsx
+++ frontend/src/pages/event/CardTickets.jsx
@@ -1,24 +1,24 @@
-import { Button, Card, CardBody } from '@heroui/react'
-import React from 'react'
+import { Button, Card, CardBody } from '@heroui/react';
+import React from 'react';
 
-function CardTIckets() {
+function CardTIckets({ fecha, logo }) {
   return (
-    <div className='flex flex-wrap w-1/3'>
-      <Card>
-        <CardBody>
-          <h3>22 de marzo de 2025 · Ensenada, Baja California</h3>
-          <Button className="flex items-center bg-white rounded-md shadow-md hover:bg-gray-100 transition">
+    <div className="w-full p-1 md:w-2/3 lg:w-2/3">
+      <Card className="w-full h-full">
+        <CardBody className="p-4 flex flex-col gap-4">
+          <h3>{fecha}</h3>
+          <Button className="w-full flex items-center gap-3 bg-white rounded-md shadow-md hover:bg-gray-100 transition px-4 py-3">
             <img
-              src="https://cdn.brandfetch.io/idVogTb1a1/w/800/h/800/theme/dark/icon.png?c=1dxbfHSJFAPEGdCLU4o5B"
-              alt="Ticketmaster Logo"
-              className="w-auto h-6 rounded-full"
+              src={logo}
+              alt="Logo"
+              className="w-auto h-6 rounded-full shrink-0"
             />
-            <span className="text-black font-medium">Comprar boletos</span>
+            <span className="text-black font-medium whitespace-nowrap">Comprar Boletos</span>
           </Button>
         </CardBody>
       </Card>
     </div>
-  )
+  );
 }
 
-export default CardTIckets
+export default CardTIckets;
