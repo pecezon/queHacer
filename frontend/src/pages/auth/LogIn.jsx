ddc570ddab815cc32f0dbc4de563b21090385506
diegopez <lopez.diego@cetys.edu.mx>
2025-04-29 19:14:14 -0700

    DIABLO PLAN

--- frontend/src/pages/auth/LogIn.jsx
+++ frontend/src/pages/auth/LogIn.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Input, Button } from "@heroui/react";
 
-const LogIn = () => {
+const LogIn = () > {
   return (
     <div className="flex justify-center items-center h-screen w-screen bg-gradient-to-r from-indigo-200 to-blue-900">
       <div className="flex flex-col justify-center items-center bg-white p-8 rounded-lg shadow-2xl w-3/4 gap-8 max-w-lg">
