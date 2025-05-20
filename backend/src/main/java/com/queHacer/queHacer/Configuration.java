41697d8d11719d9fc3b679a8785be4c345be8a07
Diego López <lopez.diego@cetys.edu.mx>
2025-05-20 01:02:34 -0700

    enable CORS

--- backend/src/main/java/com/queHacer/queHacer/Configuration.java
+++ backend/src/main/java/com/queHacer/queHacer/Configuration.java
@@ -2,6 +2,8 @@ package com.queHacer.queHacer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
@@ -9,5 +11,19 @@ public class Configuration {
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Allow CORS on all endpoints
+                        .allowedOrigins("http://localhost:3000") // Replace with your frontend's origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
